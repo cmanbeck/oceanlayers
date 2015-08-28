1ac7be6b4ad8e39b1884c8c15561aa46030ac170
Courtney <manbeckcourtney@gmail.com>
2015-08-28 14:37:27 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var layerInfo = {
+  "epipelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+  
+  "mesopelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [{"name": " " "image": " " "details": " "}],
+    "desc": "   "
+  }
+
+  "bathypelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+  "abyssopelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+  "hadalpelagic": {
+    "color": "",
+    "depth": "",
+    "creatures": [],
+    "desc": "   "
+  }
+
+}
+
+// $(function() {
+//     var img = $("#sheephead"),
+//         width = img.get(0).width,
+//         screenWidth = $(window).width(),
+//         duration = 2000;
+
+//     function animateSheephead() {
+//         img.css("right", -width).animate({
+//             "right": screenWidth
+//         }, duration, animateSheephead);
+//     }
+
+//     animateSheephead();
+// });
